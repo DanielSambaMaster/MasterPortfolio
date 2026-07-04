938328d1eea24def15fd54ab577c518d073ad481
Samba <danielsamba0000@icloud.com>
2026-07-04 03:45:50 +0100

    HeaderSet040726

--- FrontendCommerce/Oku/src/assets/Consoler.jsx
+++ FrontendCommerce/Oku/src/assets/Consoler.jsx
@@ -1,3 +1,7 @@
+
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { faSun, faMoon, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import { faZhihu } from "@fortawesome/free-brands-svg-icons"
 export default function Consoler({ mode, setMode }) {
 
     return (
@@ -15,7 +19,7 @@ export default function Consoler({ mode, setMode }) {
     return(
         <div className={mode ? "headerlight" : "headerdark"}>
 
-                <div className="header-title"><span className="header-title-text">Daniel Samba</span> <button className="header-button">Center</button></div>
+                <div className="header-title"><span className={mode ? "header-title-text-light" : "header-title-text-dark"}>Daniel Samba</span> <button className="header-button"><FontAwesomeIcon icon={faMagnifyingGlass} /></button></div>
                 <div className="header-list-container">
 
                     <ul className="header-list">
@@ -27,8 +31,8 @@ export default function Consoler({ mode, setMode }) {
                 </div>
                 <div className="header-button-container">
 
-                    <button className="header-button" onClick={() => setMode(!mode)}>{mode ? "Dark" : "Light"}</button>
-                    <button className="header-button">EN-PC</button>
+                    <button className="header-button-main" onClick={() => ChangeMode({mode, setMode})}>{mode ? <FontAwesomeIcon icon={faMoon} className="faMoon" /> : <FontAwesomeIcon icon={faSun} className="faSun"/>}</button>
+                    <button className="header-button-languages"><FontAwesomeIcon icon={faZhihu} className="faZhihu" /></button>
                 </div>
 
 
@@ -37,3 +41,12 @@ export default function Consoler({ mode, setMode }) {
         </div>
     )
   }
+  
+  function ChangeMode( {mode, setMode}) {
+        setMode(!mode);
+
+            document.body.style.backgroundColor = mode ? "black" : "#f5f5f5";
+
+
+
+    }
